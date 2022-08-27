9ec7a0e5fb45146808594fbebf26a05c12a6babc
Jesse Cooper <CLJcareer@outlook.com>
2022-08-27 16:57:21 -0500

    Add entity and migration for initial database setup

---
+++ src/main/java/com/collegeharbor/collegedataintake/CollegeDataIntakeApplication.java
@@ -0,0 +1,13 @@
+package com.collegeharbor.collegedataintake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeDataIntakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeDataIntakeApplication.class, args);
+    }
+
+}
